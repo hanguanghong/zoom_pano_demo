3f33087950b024e41936abdd11f37daa84399503
ghan <ghan@polycom.com>
2018-08-23 17:55:54 +0800

    init version

---
+++ demo/src/main/java/us/zoom/sdkexample/Constants.java
@@ -0,0 +1,23 @@
+package us.zoom.sdkexample;
+
+public interface Constants {
+
+	// TODO Change it to your web domain
+	public final static String WEB_DOMAIN = "zoom.us";
+
+	// TODO Change it to your APP Key
+	public final static String APP_KEY = "lyg6wENqBLDWwFoCGwog5C4I75a5tDhFxRiv";
+	
+	// TODO Change it to your APP Secret
+	public final static String APP_SECRET = "UsVV89yTckVaRSWJMGWJbmV2mxoYQ2kU95nh";
+
+	// TODO change it to your user ID
+	public final static String USER_ID = "T_MUzTh8Rpe3x_dcJpFSxQ";
+	
+	// TODO change it to your token
+	public final static String ZOOM_TOKEN = "uIR3uXNjzu6maeWm5YGOQr35Bhmd28Y2lKmMqGAlHgU.BgQgRXJYQjlMTEI4TVVlVU5RdVNGMnpCRkI3N1VZSTN4K2FAMzczODYwNzBmNThhZmMwMzk4OTg4NDYxYzA0MjFlOWZmNzJkYmJjNjY0MThiMGE2ZjY5YzY4NzdjYTc1ODZhZQAMM0NCQXVvaVlTM3M9AA";
+
+	// TODO change it to your access token
+	public final static String ZOOM_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjbGllbnQiLCJ1aWQiOiJUX01VelRoOFJwZTN4X2RjSnBGU3hRIiwiaXNzIjoid2ViIiwic3R5IjoxMDAsImNsdCI6MCwic3RrIjoidUlSM3VYTmp6dTZtYWVXbTVZR09RcjM1QmhtZDI4WTJsS21NcUdBbEhnVS5CZ1FnUlhKWVFqbE1URUk0VFZWbFZVNVJkVk5HTW5wQ1JrSTNOMVZaU1RONEsyRkFNemN6T0RZd056Qm1OVGhoWm1Nd016azRPVGc0TkRZeFl6QTBNakZsT1dabU56SmtZbUpqTmpZME1UaGlNR0UyWmpZNVl6WTROemRqWVRjMU9EWmhaUUFNTTBOQ1FYVnZhVmxUTTNNOUFBIiwiZXhwIjoxNTMzNzAyOTgzLCJpYXQiOjE1MzM2OTU3ODMsImFpZCI6InFyMjlPeVYxUmlDVXJWQjItR2ZNNVEiLCJjaWQiOiIifQ.2WPRiCYmkVTHvNKOySQP9mtUnkkSvx_KLSCtf8_THIY";
+
+}
