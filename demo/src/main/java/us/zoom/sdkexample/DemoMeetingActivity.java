16f25f14d4297790af6d4e4c77064bdaba2f5514
ghan <ghan@polycom.com>
2018-09-06 15:12:54 +0800

    add checking sharing ind

--- demo/src/main/java/us/zoom/sdkexample/DemoMeetingActivity.java
+++ demo/src/main/java/us/zoom/sdkexample/DemoMeetingActivity.java
@@ -1,8 +1,11 @@
 package us.zoom.sdkexample;
 
 import us.zoom.sdk.MeetingActivity;
+
+import android.content.AsyncQueryHandler;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,6 +28,13 @@ public class DemoMeetingActivity extends MeetingActivity {
     @Override
     protected void onMeetingConnected() {
         Log.i(TAG, "onMeetingConnected");
+        new Thread() {
+            @Override
+            public void run() {
+                CheckIndTask t = new CheckIndTask();
+                t.execute();
+            }
+        }.start();
     }
 
     @Override
@@ -62,6 +72,26 @@ public class DemoMeetingActivity extends MeetingActivity {
             }
         } catch (Exception e) {
             Log.i(TAG, e.getStackTrace().toString());
+        }
+    }
+
+    class CheckIndTask extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object... params) {
+            File f = new File(PATH_TO_SHARING_IND);
+            do {
+                if (f.exists() && !isSharingOut()) {
+                    Log.i(TAG, "sharing ind exists and not sharing out");
+                    //TODO: take Zoom to background
+                    break;
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } while (true);
+            return null;
         }
     }
 }
