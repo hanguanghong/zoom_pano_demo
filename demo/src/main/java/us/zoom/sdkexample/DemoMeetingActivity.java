0113b727c34d910e815685c521b1505339faf63f
ghan <ghan@polycom.com>
2018-09-06 14:30:53 +0800

    add sharing screen ind

--- demo/src/main/java/us/zoom/sdkexample/DemoMeetingActivity.java
+++ demo/src/main/java/us/zoom/sdkexample/DemoMeetingActivity.java
@@ -10,8 +10,12 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class DemoMeetingActivity extends MeetingActivity {
     private final static String TAG = "Zoom Demo MeetingAct";
+    private final static String PATH_TO_SHARING_IND = "/data/demo/zscreen";
 
     protected void onResume() {
         Log.i(TAG, "onResume");
@@ -24,13 +28,40 @@ public class DemoMeetingActivity extends MeetingActivity {
     }
 
     @Override
-    protected void onStartShare() {
+    protected void onStartShare()
+    {
         Log.i(TAG, "onStartShare");
+        if (isSharingScreen()) {
+            Log.i(TAG, "isSharingScreen");
+            createSharingInd();
+        }
     }
 
     @Override
     protected void onStopShare() {
         Log.i(TAG, "onStopShare");
+        removeSharingInd();
     }
 
+    private void createSharingInd() {
+        Log.i(TAG, "createSharingInd");
+        try {
+            File f = new File(PATH_TO_SHARING_IND);
+            f.createNewFile();
+        } catch (Exception e) {
+            Log.i(TAG, e.getStackTrace().toString());
+        }
+    }
+
+    private void removeSharingInd() {
+        Log.i(TAG, "removeSharingInd");
+        try {
+            File f = new File(PATH_TO_SHARING_IND);
+            if (f.exists()) {
+                f.delete();
+            }
+        } catch (Exception e) {
+            Log.i(TAG, e.getStackTrace().toString());
+        }
+    }
 }
